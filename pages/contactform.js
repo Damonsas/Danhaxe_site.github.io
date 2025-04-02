e9a43f273b565678f68f72d1fed1b4d545839a5c
Damonsas <danh.kh.pro@gmail.com>
2025-04-02 10:42:58 +0200

    up

--- pages/contactform.js
+++ pages/contactform.js
@@ -1,4 +1,9 @@
 
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-link ul').classList.toggle('active');
+});
+
+
 <form onsubmit="return false"></form>
 
   
