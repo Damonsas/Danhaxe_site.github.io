87cd465883a87740a4d85f01dbc3ec9f1e3d33fc
Damonsas <danh.kh.pro@gmail.com>
2025-04-09 13:53:49 +0200

    s

--- pages/contactform.js
+++ pages/contactform.js
@@ -1,6 +1,10 @@
 
 document.querySelector('.menu-toggle').addEventListener('click', () => {
+    console.log('Menu toggle clicked');
     document.querySelector('.nav-link ul').classList.toggle('active');
 });
+
+
+
 
   
